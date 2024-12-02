51fb30b598ece23cbd3843d902597843579de857
adorable-otter <plan.stack@gmail.com>
2024-12-02 12:30:51 +0900

    fix: 로그인 기능 관련 코드 주석처리

--- src/api/user/auth.js
+++ src/api/user/auth.js
@@ -27,29 +27,28 @@ const addUserInfo = async (userInfo) => {
   if (error) throw error;
 };
 
-export const requestLogin = async (authInfo) => {
-  const { data, error } = await supabase.auth.signInWithPassword(authInfo);
-  if (error) throw error;
-  return data.user;
-};
+// export const requestLogin = async (authInfo) => {
+//   const { data, error } = await supabase.auth.signInWithPassword(authInfo);
+//   if (error) throw error;
+//   return data.user;
+// };
 
-export const fetchUserInfoByUserName = async (userName) => {
+const fetchUserInfoByUserName = async (userName) => {
   const { data, error } = await supabase
     .from('users')
     .select()
     .eq('user_name', userName)
     .maybeSingle();
-  console.log('🚀 ~ fetchUserInfoByUserName ~ error:', error);
   if (error) throw error;
   return data;
 };
 
-export const fetchUserInfoById = async (id) => {
-  const { data, error } = await supabase
-    .from('users')
-    .select()
-    .eq('id', id)
-    .maybeSingle();
-  if (error) throw error;
-  return data;
-};
+// export const fetchUserInfoById = async (id) => {
+//   const { data, error } = await supabase
+//     .from('users')
+//     .select()
+//     .eq('id', id)
+//     .maybeSingle();
+//   if (error) throw error;
+//   return data;
+// };
