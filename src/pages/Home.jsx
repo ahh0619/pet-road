493cbb9306fbd5fcef1e9a0afb0a4faf9879aa6b
Eunyeong Park <eunyeong.cloud@gmail.com>
2024-12-03 10:47:29 +0900

    add: datail 페이지 추가 및 home 컴포넌트 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import Detail from '../components/detail/Detail';
+import DetailComponent from '../components/home/DetailComponent';
 import KakaoMap from '../components/home/KakaoMap';
+import MainContentsComponent from '../components/home/MainContentsComponent';
+import SideBarComponent from '../components/home/SideBarComponent';
 
 const Home = () => {
   return (
     <div>
       <KakaoMap />
-      <Detail />
+      <SideBarComponent />
+      <MainContentsComponent />
+      <DetailComponent />
     </div>
   );
 };
