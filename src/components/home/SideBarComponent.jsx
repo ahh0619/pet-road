13bf05006c3da41be4adb6fbedf4496f8e4f5619
Eunyeong Park <eunyeong.cloud@gmail.com>
2024-12-03 16:59:18 +0900

    update: component 이동 및 열기, 열기 닫기

--- src/components/home/SideBarComponent.jsx
+++ src/components/home/SideBarComponent.jsx
@@ -6,19 +6,28 @@ import {
 } from '../../styles/KakaoMapStyle';
 import { Hr } from '../../styles/PubLoginStyle';
 
-const SideBarComponent = () => {
+const SideBarComponent = ({showMain, setShowMain, showBookmark, setShowBookmark}) => {
+
+  const handleHomeIcon = () => {
+    if(showMain){ setShowMain(false); } else { setShowMain(true) }
+  }
+
+  const handleBookmarkIcon = () => {
+    if(showBookmark){ setShowBookmark(false); } else { setShowBookmark(true) }
+  }
+
   return (
     <SideBar>
       <SideBarLogoWrap>
         <img src="/images/map-pet-logo.png" />
       </SideBarLogoWrap>
-      <Hr bottomGap="false" />
-      <Tab selectOn="true">
+      <Hr $bottomGap="false" />
+      <Tab $selectOn="true" onClick={handleHomeIcon}>
         <i className="fa-solid fa-location-dot"></i>
         <br />
         <p>지도 홈</p>
       </Tab>
-      <Tab>
+      <Tab onClick={handleBookmarkIcon}>
         <i className="fa-solid fa-heart"></i>
         <br />
         <p>북마크</p>
